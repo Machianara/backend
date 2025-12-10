a8645e1ba98954e589de627e4a8865fa48a3e044
Ranfl <rasyidnaufal54@gmail.com>
2025-12-10 13:49:57 +0700

    Fix leftover conflict in manualinputRoutes

--- src/routes/manualinputRoutes.js
+++ src/routes/manualinputRoutes.js
@@ -1,18 +1,9 @@
 import express from "express";
 import { manualInputMachine } from "../controllers/manualinputController.js";
-<<<<<<< HEAD
 
 const router = express.Router();
 
 // POST /manual-input
 router.post("/", manualInputMachine);
-=======
-import { verifyToken } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-// POST /manual-input - require token
-router.post("/", verifyToken, manualInputMachine);
->>>>>>> origin/feature/admin
 
 export default router;
