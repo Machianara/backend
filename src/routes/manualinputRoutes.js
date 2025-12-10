a200efd77e12d709a157271b5c101bff301c9a89
Ranfl <rasyidnaufal54@gmail.com>
2025-12-10 14:47:23 +0700

    fix lil bug

--- src/routes/manualinputRoutes.js
+++ src/routes/manualinputRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { manualInputMachine } from "../controllers/manualinputController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// POST /manual-input
+// POST /manual-input (with token)
 router.post("/", verifyToken, manualInputMachine);
 
 export default router;
